946e95864fd42becef1bb01c24eedd482e75d2a8
Dmitriy Litmanovich <dlitmanovic@gmail.com>
2022-07-27 22:52:10 +0300

    After P2P

--- HW_2_python/get-movies.py
+++ HW_2_python/get-movies.py
@@ -9,14 +9,13 @@ def get_argpars():
     parser = argparse.ArgumentParser()
     parser.add_argument('--N', type=int, help='the number of movies you want to select')
     parser.add_argument('--genres', type=str, help='enter the genres you want to see', default='')
-    parser.add_argument('--year_from', type=int,  help='enter the year from', default=1900)
-    parser.add_argument('--year_to', type=int,  help='enter the year to', default=2023)
-    parser.add_argument('--regexp',  type=str,  help='search movie by matching title',  default='')
+    parser.add_argument('--year_from', type=int, help='enter the year from', default=1900)
+    parser.add_argument('--year_to', type=int, help='enter the year to', default=2023)
+    parser.add_argument('--regexp', type=str, help='search movie by matching title', default='')
     return parser.parse_args()
 
 
 def get_data_from_csv(file_path: str):
-
     with open(file_path, encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         data = list(reader)
@@ -30,7 +29,7 @@ def get_the_averages():
     for line in get_data_from_csv('files/ratings.csv'):
         total[line[1]] += float(line[2])
         count[line[1]] += 1
-    averages = {id: total[id]/count[id] for id in count}
+    averages = {id: total[id] / count[id] for id in count}
     return averages
 
 
@@ -72,7 +71,6 @@ def search_by_genres(genre, arg_genre: str):
 
 
 def get_and_check_rating(movies_id: int, rating_dict: dict):
-
     if movies_id in rating_dict:
         return round(rating_dict[movies_id], 1)
     else:
@@ -91,12 +89,12 @@ def check_by_n(N: int, genre: str, count: Counter):
 
 
 def sorted_list_line(data: list):
-
     for movie_id, movie_name, movies_genres in data:
         if check_year_in_range(args.year_from, args.year_to, movie_name) and check_by_regexp(args.regexp, movie_name):
             for arg_genre in args.genres.split('|'):
                 for genre in movies_genres.split('|'):
                     if search_by_genres(genre, arg_genre):
+                        # removing the last 7 characters to get rid of (****)
                         name = movie_name[:-7]
                         year = int(re.search(r'\((\d{4})\)', movie_name).group(0)[1:-1])
                         yield movie_id, genre, name, year
